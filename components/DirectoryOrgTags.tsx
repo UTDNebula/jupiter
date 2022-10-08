3023fdbc831a2676c0bfea44ea8adfb1e927cd78
Ruben Olano <rubendavid2711@gmail.com>
2022-10-08 17:55:32 -0500

    Update grid to be more responsive and further break down org cards and org tags with small updates

---
+++ components/DirectoryOrgTags.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+
+interface Props {
+  tag: string;
+}
+
+const DirectoryOrgTags: FC<Props> = ({ tag }) => {
+  return (
+    <div className="flex justify-between items-center md:w-auto h-8 m-1 text-xs md:text-sm p-4 font-light rounded-2xl bg-blue-400 hover:bg-blue-500 hover:cursor-pointer">
+      {tag}
+    </div>
+  );
+};
+
+export default DirectoryOrgTags;
--- pages/organization.tsx
+++ pages/organization.tsx
@@ -1,25 +1,88 @@
-import React from "react";
-import ContactList from "../components/ContactList";
+import React from 'react';
+import ContactList from '../components/ContactList';
 import orgData from '../demoOrgPageData.json';
 
 export default function Directory() {
-    return(
+  return (
+    <div>
+      <p
+        style={{
+          position: 'relative',
+          paddingLeft: 20,
+          paddingTop: 10,
+          fontSize: 35,
+          fontWeight: '450',
+        }}
+      >
+        {orgData.name}
+      </p>
+      <div
+        style={{
+          justifyItems: 'left',
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr 1fr',
+        }}
+      >
         <div>
-            <p style={{position: 'relative', paddingLeft: 20, paddingTop: 10, fontSize:35, fontWeight:"450"}}>{orgData.name}</p>
-            <div style={{justifyItems:"left", display:"grid",  gridTemplateColumns:"1fr 1fr 1fr"}}>
-                <div>
-            <p style={{position: 'relative', paddingLeft: 20, paddingTop: 10, fontSize:25, fontWeight:"450"}} >About Us</p>
-            <div className="detail-org-image-container">
-                <div className="detail-org-image"/>
-            </div>
-            </div>
-            <p style={{paddingTop:40, fontSize:14}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec ligula vel lacus iaculis egestas. Donec tincidunt bibendum mattis. Morbi leo lorem, dapibus eget purus vitae, pellentesque porta tortor. Duis molestie molestie venenatis. Sed id porttitor ante. Ut pretium, sapien sed venenatis tristique, eros nunc vulputate elit, eu malesuada lacus metus sit amet urna. Vivamus placerat eros ipsum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; <br /> Aliquam sed nisi sed nunc vestibulum vestibulum. Integer at aliquet augue, dapibus facilisis nisi. Maecenas sit amet rhoncus magna. Mauris fringilla vel lorem nec luctus. Donec tincidunt ante et mi malesuada bibendum. In mattis porta nisl ut tincidunt. Aenean vel orci erat. Duis feugiat augue purus, vel mattis odio maximus finibus. Etiam est justo, semper quis mattis vel, mattis sed orci. Ut sed facilisis magna, eget sagittis metus. Proin in pharetra mauris, id egestas ligula. Donec venenatis arcu mauris, et tempus massa convallis id. Donec feugiat a ante id suscipit. Nulla condimentum non odio in feugiat. Donec euismod risus in ipsum dignissim pharetra.</p>
-                <div style={{paddingLeft:20}}>
-                 <p style={{position: 'relative', paddingTop: 10, fontSize:25, fontWeight:"450"}}>Contact</p>
-                <ContactList contactMethods={["Discord", "Instagram", "Email"]} />
-                </div>
-            </div>
-            <p style={{position: 'relative', paddingLeft: 20, paddingTop: 20, fontSize:25, fontWeight:"400"}}>Upcoming Events</p>
+          <p
+            style={{
+              position: 'relative',
+              paddingLeft: 20,
+              paddingTop: 10,
+              fontSize: 25,
+              fontWeight: '450',
+            }}
+          >
+            About Us
+          </p>
+          <div className="detail-org-image-container">
+            <div className="detail-org-image" />
+          </div>
         </div>
-    )
+        <p style={{ paddingTop: 40, fontSize: 14 }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec
+          ligula vel lacus iaculis egestas. Donec tincidunt bibendum mattis.
+          Morbi leo lorem, dapibus eget purus vitae, pellentesque porta tortor.
+          Duis molestie molestie venenatis. Sed id porttitor ante. Ut pretium,
+          sapien sed venenatis tristique, eros nunc vulputate elit, eu malesuada
+          lacus metus sit amet urna. Vivamus placerat eros ipsum. Vestibulum
+          ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+          curae; <br /> Aliquam sed nisi sed nunc vestibulum vestibulum. Integer
+          at aliquet augue, dapibus facilisis nisi. Maecenas sit amet rhoncus
+          magna. Mauris fringilla vel lorem nec luctus. Donec tincidunt ante et
+          mi malesuada bibendum. In mattis porta nisl ut tincidunt. Aenean vel
+          orci erat. Duis feugiat augue purus, vel mattis odio maximus finibus.
+          Etiam est justo, semper quis mattis vel, mattis sed orci. Ut sed
+          facilisis magna, eget sagittis metus. Proin in pharetra mauris, id
+          egestas ligula. Donec venenatis arcu mauris, et tempus massa convallis
+          id. Donec feugiat a ante id suscipit. Nulla condimentum non odio in
+          feugiat. Donec euismod risus in ipsum dignissim pharetra.
+        </p>
+        <div style={{ paddingLeft: 20 }}>
+          <p
+            style={{
+              position: 'relative',
+              paddingTop: 10,
+              fontSize: 25,
+              fontWeight: '450',
+            }}
+          >
+            Contact
+          </p>
+          <ContactList contactMethods={['Discord', 'Instagram', 'Email']} />
+        </div>
+      </div>
+      <p
+        style={{
+          position: 'relative',
+          paddingLeft: 20,
+          paddingTop: 20,
+          fontSize: 25,
+          fontWeight: '400',
+        }}
+      >
+        Upcoming Events
+      </p>
+    </div>
+  );
 }
