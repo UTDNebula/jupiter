c9769883c431963ade48958eabb3c11dbb489431
Ruben Olano <rubendavid2711@gmail.com>
2022-10-08 17:55:32 -0500

    Update grid to be more responsive and further break down org cards and org tags with small updates

--- components/OrgDirectoryHeader.tsx
+++ components/OrgDirectoryHeader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const OrgDirectoryHeader = () => {
   return (
-    <div className="flex col-start-2 items-start h-full p-3 col-end-[-1] row-start-1 text-5xl font-bold mb-5 w-full justify-between">
+    <div className="flex col-start-2 items-start h-full p-3 col-end-[-1] row-start-1 md:text-3xl lg:text-5xl font-bold mb-5 w-full justify-between">
       <h1>Organization Directory</h1>
       <input
         className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focuse:outline-blue-500"
---
+++ types/orgs.tsx
@@ -0,0 +1,5 @@
+export interface Org {
+  name: string;
+  imageLink: string;
+  tags: string[];
+}
