3eb71e42894fd5edac38bd3a901bbe4ca9845a68
Jake Spann <jake323@comcast.net>
2022-10-11 14:41:41 -0500

    Update image links & add local images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { domains: ['about.utdnebula.com'] },
+  images: {
+    domains: ['about.utdnebula.com', 'utdallas-cdn.presence.io', `github.com`],
+  },
 };
 
 module.exports = nextConfig;
