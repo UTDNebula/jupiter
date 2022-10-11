bbb4a351296d3b17eb9a40bd9e59d2345cf6c2d3
Ruben Olano <rubendavid2711@gmail.com>
2022-10-10 19:54:58 -0500

    Rename X element to XButton to improve clarity

---
+++ components/OrgDirectorySidebar.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const DropDownIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="w-5 text-gray-400 hover:cursor-pointer"
+    viewBox="0 0 20 20"
+    fill="currentColor"
+  >
+    <path
+      fillRule="evenodd"
+      d="M5.293 7.293a1 1 0 011.414 0L10 11.586l4.293-4.293a1 1 0 111.414 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+
+const OrgDirectorySidebar = () => {
+  return (
+    <div className="md:flex hidden flex-col md:col-start-1 md:col-end-2 md:row-start-2 md:row-end-2 w-full mb-5">
+      <div className="flex flex-col mb-5 text-2xl font-ligh">
+        <h3>Filters</h3>
+      </div>
+      <div className="flex flex-col mb-5 text-xl font-light border-y-2 border-gray-300">
+        <div className="flex flex-row justify-between align-middle">
+          <h4>Tags</h4>
+          <DropDownIcon />
+        </div>
+      </div>
+      <div className="flex flex-col mb-5 text-xl font-light border-y-2 border-gray-300">
+        <div className="flex flex-row justify-between align-middle">
+          <h4>Category</h4>
+          <DropDownIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrgDirectorySidebar;
