1849726150be5f1d52c430f1db5cac1e5dea773c
Ruben Olano <rubendavid2711@gmail.com>
2022-10-10 19:54:58 -0500

    Rename X element to XButton to improve clarity

--- components/OrgDirectorySidebar.tsx
+++ components/OrgDirectorySidebar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const DropDown = () => (
+const DropDownIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     className="w-5 text-gray-400 hover:cursor-pointer"
@@ -24,13 +24,13 @@ const OrgDirectorySidebar = () => {
       <div className="flex flex-col mb-5 text-xl font-light border-y-2 border-gray-300">
         <div className="flex flex-row justify-between align-middle">
           <h4>Tags</h4>
-          <DropDown />
+          <DropDownIcon />
         </div>
       </div>
       <div className="flex flex-col mb-5 text-xl font-light border-y-2 border-gray-300">
         <div className="flex flex-row justify-between align-middle">
           <h4>Category</h4>
-          <DropDown />
+          <DropDownIcon />
         </div>
       </div>
     </div>
