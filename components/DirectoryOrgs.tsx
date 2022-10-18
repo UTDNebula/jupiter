d0eaa7826d1ea84846a61b4fc7c8d67c9a8a9a08
Ruben Olano <rubendavid2711@gmail.com>
2022-10-18 09:36:27 -0500

    resolve some merge conflicts

--- components/DirectoryOrgs.tsx
+++ components/DirectoryOrgs.tsx
@@ -8,11 +8,7 @@ interface Props {
 }
 const OrgDirectoryCards: FC<Props> = ({ org }) => {
   return (
-<<<<<<< HEAD
-    <div className="md:w-full h-auto flex flex-col justify-between items-center p-5 m-5 rounded-2xl">
-=======
     <div className="w-full h-auto flex flex-col justify-between items-center p-5 m-5 rounded-2xl bg-white shadow-md hover:shadow-lg hover:cursor-pointer">
->>>>>>> cc11936 (Update grid to be more responsive and further break down org cards and org tags with small updates)
       <h4 className="text-lg font-bold text-center">{org.name}</h4>
       <div className="w-full h-40 relative justify-center">
         <Image
@@ -23,11 +19,7 @@ const OrgDirectoryCards: FC<Props> = ({ org }) => {
           className="rectangle"
         />
       </div>
-<<<<<<< HEAD
-      <div className="flex flex-wrap content-start justify-center h-[5vh] w-full">
-=======
       <div className="flex flex-wrap md:flex-row align-middle justify-center">
->>>>>>> cc11936 (Update grid to be more responsive and further break down org cards and org tags with small updates)
         {org.tags.map((tag, i) => (
           <DirectoryOrgTags key={i} tag={tag} />
         ))}
