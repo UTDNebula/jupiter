3b7633ca1f8e04bdd94006d5dfd329e86abf5f9a
Ruben Olano <rubendavid2711@gmail.com>
2022-11-01 20:00:42 -0500

    Directory header no longer in two lines

--- components/OrgDirectoryHeader.tsx
+++ components/OrgDirectoryHeader.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const OrgDirectoryHeader = () => {
   return (
     <div className="md:col-start-2 md:col-end-[-1] grid md:grid-cols-3">
-      <h1 className="align-middle text-3xl font-bold text-gray-800 col-start-1">
+      <h1 className="align-middle text-3xl font-bold text-gray-800 col-start-1 col-span-3">
         Organization Directory
       </h1>
       <div className="md:col-start-[-1]">
