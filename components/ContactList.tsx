c12fa7cbf87a64abfd97fb132a763379b17f2c06
Jake <Jake3231@users.noreply.github.com>
2023-02-13 13:30:37 -0600

    Support opening links when clicking contact button

--- components/ContactList.tsx
+++ components/ContactList.tsx
@@ -10,12 +10,14 @@ const ContactList: FC<ContactListProps> = ({ contactMethods }) => {
   return (
     <>
       {contactMethods.map((contact, i) => (
+        <a href="https://www.utdnebula.com">
         <div
           key={i}
           className="flex flex-row justify-center items-center p-3 m-3 rounded-2xl bg-gray-200 hover:bg-gray-300 cursor-pointer"
           style={{backgroundColor:(contactStyles[contact] ?? contactStyles["Default"]).bg, color:(contactStyles[contact] ?? contactStyles["Default"]).text}}  >
           <p className="text-2xl font-bold">{contact}</p>
         </div>
+        </a>
       ))}
     </>
   );
