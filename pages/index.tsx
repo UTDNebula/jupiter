3764e3fc57e1cb7eb230af10e88cc012130b0c84
Ruben Olano <rubendavid2711@gmail.com>
2023-03-20 23:27:23 -0500

    SOme testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
+import DirectorySearch from './../DirectorySearch';
 import Head from 'next/head';
 import Image from 'next/image';
 import EventCard from '../components/EventCard';
 import banner from '../public/banner.png';
 export default function Home() {
   const events = ['Event 1', 'Event 2', 'Event 3', 'Event 4'];
-  
+
   return (
     <>
       <Head>
@@ -24,18 +25,7 @@ export default function Home() {
             priority
           />
         </div>
-        <div className="justify-center flex flex-col min-h-[45vh] mx-auto md:w-[55vw] w-[75vw]">
-          <p className="relative text-6xl md:text-8xl font-semibold text-white">
-            Jupiter
-          </p>
-          <p className="relative text-base md:text-2xl font-semibold text-white">
-            Get Connected on Campus.
-          </p>
-          <input
-            className="relative mt-5 md:mt-10 rounded-md md:text-2xl w-full h-12 md:h-16 px-4 md:px-8 bg-white"
-            placeholder="Search Organizations"
-          />
-        </div>
+        <DirectorySearch />
         <div className="p-8">
           <h1 className="font-medium text-2xl">Events</h1>
           <div className="grid grid-cols-4 grid-flow-row gap-4 w-full h-max">
