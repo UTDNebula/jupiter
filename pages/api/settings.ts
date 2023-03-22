b178609c02d8e8b5c342df443ddbcb863b3de04f
Ruben Olano <rubendavid2711@gmail.com>
2023-03-22 15:53:15 -0500

    Additionaly checks in api function

--- pages/api/settings.ts
+++ pages/api/settings.ts
@@ -12,37 +12,42 @@ const Handler = async (
   res: NextApiResponse,
   db_provider = new DbProvider(),
 ) => {
-  // MAKE SURE TO CHECK IF THE USER IS AUTHENTICATED BEFORE
-  const session = await getServerSession(req, res, authOps);
-  if (!session) return res.status(401).json({ error: 'Unauthorized' });
+  try {
+    // MAKE SURE TO CHECK IF THE USER IS AUTHENTICATED BEFORE
+    const session = await getServerSession(req, res, authOps);
+    if (!session) return res.status(401).json({ error: 'Unauthorized' });
 
-  const { user_id } = req.query;
-  const user = JSON.parse(req.body);
+    const { user_id } = req.query;
+    const user = JSON.parse(req.body);
 
-  // Check that the user_id and user are present
-  if (!user_id || !user)
-    return res.status(400).json({ error: 'Missing user_id or user' });
+    // Check that the user_id and user are present
+    if (!user_id || !user)
+      return res.status(400).json({ error: 'Missing user_id or user' });
 
-  // Check that the user_id is a string
-  if (typeof user_id !== 'string')
-    return res.status(400).json({ error: 'user_id must be a string' });
+    // Check that the user_id is a string
+    if (typeof user_id !== 'string')
+      return res.status(400).json({ error: 'user_id must be a string' });
 
-  // Check that user is an object and is a valid partial user
-  if (!isUser(user))
-    return res.status(400).json({ error: 'user must be a valid user' });
+    // Check that user is an object and is a valid partial user
+    if (!isUser(user))
+      return res.status(400).json({ error: 'user must be a valid user' });
 
-  // Fetch the user from the database
-  const dbUser = await db_provider.getUser(user_id);
+    // Fetch the user from the database
+    const dbUser = await db_provider.getUser(user_id);
 
-  // Check that the user exists
-  if (!dbUser) return res.status(400).json({ error: 'Could not find user' });
+    // Check that the user exists
+    if (!dbUser) return res.status(400).json({ error: 'Could not find user' });
 
-  // Update the user in the database
-  if (await db_provider.updateUser(user_id, user))
-    return res.status(200).json({ success: 'Successfully updated' });
+    // Update the user in the database
+    if (await db_provider.updateUser(user_id, user))
+      return res.status(200).json({ success: 'Successfully updated' });
 
-  // If the user could not be updated, return an error
-  return res.status(400).json({ error: 'Could not update user' });
+    // If the user could not be updated, return an error
+    return res.status(400).json({ error: 'Could not update user' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
 };
 
 function isUser(user: unknown): user is Partial<User> {
