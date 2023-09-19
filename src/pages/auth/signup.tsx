f9a6df502fee1fca588ee40b6ff7e9dda2d851fc
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2023-09-19 11:18:18 -0500

    Sign in and sign up pages

---
+++ src/pages/auth/signup.tsx
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import type {
+  GetServerSidePropsContext,
+  InferGetServerSidePropsType,
+} from 'next';
+import { getProviders, signIn } from 'next-auth/react';
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@src/server/auth';
+import AuthIcons from '@src/icons/AuthIcons';
+import Link from 'next/link';
+
+export default function SignIn({
+  providers,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <main className="relative flex h-screen flex-col items-center justify-center space-y-10 bg-[#ffffff] md:pl-72">
+      <h1 className="text-2xl text-slate-800">Sign up</h1>
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button onClick={() => signIn(provider.id)}>
+            {AuthIcons[provider.id]}
+          </button>
+        </div>
+      ))}
+      <h4>
+        Already have an account?{' '}
+        <Link
+          className="font-semibold text-slate-500 hover:underline"
+          href={'/auth/signin'}
+        >
+          {'Sign in'}
+        </Link>
+      </h4>
+    </main>
+  );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  if (session) {
+    return { redirect: { destination: '/' } };
+  }
+
+  const providers = await getProviders();
+
+  return {
+    props: { providers: providers ?? [] },
+  };
+}
