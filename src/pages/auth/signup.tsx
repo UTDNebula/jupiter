a5873aaef46bbff086272c83ba28145f252427b4
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2023-09-21 12:15:15 -0500

    Disable eslint rules

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-misused-promises */
+
 import type {
   GetServerSidePropsContext,
   InferGetServerSidePropsType,
