c93d9465433d8d0253ab54a153316772f733614c
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2023-09-27 14:04:08 -0500

    Display time

--- src/components/EventCalendarCard.tsx
+++ src/components/EventCalendarCard.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import type { Event } from '@src/types/event';
 
 const EventCalendarCard = ({ event }: { event: Event }) => {
+  const time = `${event.startTime.getHours()}:${event.startTime.getMinutes()}`;
+
   return (
     <div className="m-2 w-36 select-none items-center rounded-lg bg-events p-2 text-center">
       <div className="-m-2 flex justify-between p-2">
         <p className="text-xs font-semibold">{event.name}</p>
-        <p className="text-xs">11:00</p>
+        <p className="text-xs">{time}</p>
       </div>
       <p className="mt-2 line-clamp-1 text-xs">{event.description}</p>
     </div>
