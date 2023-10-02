3d11ce1912073c6aa8bed6a284a628c50f5678b0
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2023-10-01 19:48:50 -0500

    Display user profile pic

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,37 @@
+/* eslint-disable react/jsx-no-undef */
 import { type ReactNode } from 'react';
 import { ClubSearchBar, EventSearchBar } from './SearchBar';
+import Link from 'next/link';
+import { useSession, signOut } from 'next-auth/react';
+import Image from 'next/image';
 
 const BaseHeader = ({ children }: { children: ReactNode }) => {
+  const { data: session, status } = useSession();
+
   return (
     <div className="flex h-20 w-full flex-row content-between items-center justify-start px-5 py-2.5">
       {children}
       <div className="ml-auto flex items-center justify-center">
-        <div className="h-10 w-10 rounded-full bg-gray-300"></div>
+        {status === 'authenticated' ? (
+          <div className="h-10 w-10 rounded-full">
+            {session.user.image !== '' && session.user.image ? (
+              <Image
+                src={session.user.image || ''}
+                className="h-10 w-10 rounded-full bg-gray-300"
+                alt=""
+                height={100}
+                width={100}
+              />
+            ) : (
+              <div className="h-10 w-10 rounded-full bg-gray-300" />
+            )}
+          </div>
+        ) : (
+          <>
+            <Link href="/auth">Sign in</Link>
+            <Link href="/auth">Sign up</Link>
+          </>
+        )}
       </div>
     </div>
   );
