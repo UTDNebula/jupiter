0fd37c84383a477ce329d3660ab097685a711095
Kenneth <kennethimphean@gmail.com>
2023-10-09 15:13:44 -0500

    Implemented temporary placeholder page for feedback, history, liked, and settings features. No longer gives a 404 error for each page.

---
+++ src/pages/settings.tsx
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import Header from '../components/Header';
+import Image from 'next/image';
+import React from 'react';
+
+const Settings = () => {
+  return (
+    <>
+      <Head>
+        <title>Jupiter</title>
+        <meta name="description" content="Settings - Jupiter" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+			<main className="h-full md:pl-72">
+				<Header />
+				<div className="w-full pt-20 flex items-center place-content-center">
+            <Image
+              src="/nebula-logo.png"
+              alt = ""
+              width={300}
+              height={300}
+            />
+				</div>
+				<div className="h-full">
+          <h1 className=" pt-5 text-3xl font-bold text-black-500 text-center">
+            The Settings Page is Under Construction!
+					</h1>
+					<h1 className=" pt-2 text-2xl font-bold text-black-500 text-center">
+            Please Come Back Later!
+					</h1>
+				</div>
+			</main>
+		</>
+  );
+};
+
+export default Settings;
