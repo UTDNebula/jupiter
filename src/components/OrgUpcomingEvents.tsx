6089e9e32d2b21e07ec58e7b8a1ad1165c28b3ec
MistahSanta <jonathanle1111@gmail.com>
2023-10-13 01:13:59 -0500

    placeholder-text

--- src/components/OrgUpcomingEvents.tsx
+++ src/components/OrgUpcomingEvents.tsx
@@ -5,22 +5,30 @@ import { SelectClub as Club } from "@src/server/db/models";
 const cur_time = new Date();
 
 const OrgUpcomingEvents:FC<{club : Club}> = ({club}) => {
+
+  const datt = new Date("2025, 5, 4")
   
     const query_events = (api.event.byClubId.useQuery({ clubId: club.id, currentTime: cur_time, sortByDate: true  })).data
-    
+    console.log(query_events)
     return (
       <div className="w-full rounded-lg bg-slate-100 p-10">
         <h1 className="font-medium text-2xl mt-5">
           Upcoming Events
         </h1>
         <div className="flex flex-col items-start justify-evenly  md:flex-row mt-5">
-            {query_events?.map( (event) => (
+          
+            {( query_events && query_events.length > 0) 
+            ?
+             query_events.map( (event) => (
                 <div key={event.id} className=" bg-blue-500 p-4 rounded-lg cursor-pointer" >
                   <div className="font-medium">{event.name}</div>
                   <div>- {event.description}</div>
-                  <div>- {`${event.startTime.getMonth() + 1}/${event.startTime.getDay()}`}</div>
+                  <div>- {`${event.startTime.getMonth() + 1}/${event.startTime.getDate()}`}</div>
                 </div>
-            ))}
+            ))
+            :
+             <div className="font-medium">There is no upcoming events</div>
+          }
             
         </div>
       </div>
