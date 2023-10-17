60f83db21b1623e9312dc75075252bcdec553901
MistahSanta <jonathanle1111@gmail.com>
2023-10-17 10:23:51 -0500

    destructured query

--- src/components/OrgUpcomingEvents.tsx
+++ src/components/OrgUpcomingEvents.tsx
@@ -5,13 +5,8 @@ import { useState} from "react";
 const OrgUpcomingEvents:FC<{club_id : string}> = ({club_id}) => {
 
     const [cur_time] = useState( new Date() );
-    const clubDetails = {
-      clubId: club_id,
-      currentTime: cur_time,
-      sortByDate: true
-    } 
 
-    const query_events = (api.event.byClubId.useQuery( clubDetails )).data
+    const {data} = api.event.byClubId.useQuery( {clubId: club_id, currentTime: cur_time, sortByDate: true})
 
     return (
       <div className="w-full rounded-lg bg-slate-100 p-10">
@@ -20,9 +15,9 @@ const OrgUpcomingEvents:FC<{club_id : string}> = ({club_id}) => {
         </h1>
         <div className="flex flex-col items-start justify-evenly  md:flex-row mt-5">
           
-            {( query_events && query_events.length > 0) 
+            {( data && data.length > 0) 
             ?
-             query_events.map( (event) => (
+             data.map( (event) => (
                 <div key={event.id} className=" bg-blue-500 p-4 rounded-lg cursor-pointer" >
                   <div className="font-medium">{event.name}</div>
                   <div>- {event.description}</div>
