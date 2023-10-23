076260c5ca5c899ff2af0af468be781218f261a8
Ethan Bickel <notethanbickel@gmail.com>
2023-10-23 15:11:37 -0500

    disabled unused inputs

--- src/pages/manage/create.tsx
+++ src/pages/manage/create.tsx
@@ -24,26 +24,32 @@ const Page = () => {
             <h2>Description</h2>
             <textarea id="desc" className="h-24 w-full" />
           </div>
-          <div className="w-full rounded-md bg-slate-100 p-5 shadow-sm">
-            <h2>Images</h2>
-            <h3>profile picture</h3>
-            <input type="file" />
-            <h3>header image</h3>
-            <input type="file" />
-          </div>
+          {false && (
+            <div className="w-full rounded-md bg-slate-100 p-5 shadow-sm">
+              <h2>Images</h2>
+              <h3>profile picture</h3>
+              <input type="file" />
+              <h3>header image</h3>
+              <input type="file" />
+            </div>
+          )}
           <div className="w-full rounded-md bg-slate-100 p-5 shadow-sm">
             <OfficerSelector />
           </div>
-          <div className="w-full rounded-md bg-slate-100 p-5 shadow-sm">
-            <div className="flex flex-row">
-              <h2>Documents</h2>
-              <button className="ml-auto">Add Document</button>
+          {false && (
+            <div className="w-full rounded-md bg-slate-100 p-5 shadow-sm">
+              <div className="flex flex-row">
+                <h2>Documents</h2>
+                <button className="ml-auto">Add Document</button>
+              </div>
             </div>
-          </div>
-          <div className="w-full rounded-md bg-slate-100 p-5 shadow-sm">
-            <h2>Tags</h2>
-            <TagSearchBar />
-          </div>
+          )}
+          {false && (
+            <div className="w-full rounded-md bg-slate-100 p-5 shadow-sm">
+              <h2>Tags</h2>
+              <TagSearchBar />
+            </div>
+          )}
           <div className="w-full rounded-md bg-slate-100 p-5 shadow-sm">
             <ContactSelector />
           </div>
