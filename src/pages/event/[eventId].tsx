537dbcbc4b6b52339349d8924c3ddb2f8b608a83
Ruben Olano <rubendavid2711@gmail.com>
2023-11-02 16:50:08 -0500

    Revert "Update to use server side props"
    This reverts commit 2492f061d3e047571a8dad02d7295a33f4bc862a.

--- src/pages/event/[eventId].tsx
+++ src/pages/event/[eventId].tsx
@@ -1,90 +1,114 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { EventHeader } from '@src/components/Header';
+import Head from "next/head";
+import Image from "next/image";
+import { EventHeader } from "@src/components/Header";
 
 const EventPage = ({
-  club,
-  event,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-  if (!event) {
-    return <div>Event Not Found.</div>;
-  } else if (!club) {
-    return <div>Club Does Not Exist.</div>;
-  }
+    eventId
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+    const { data: event } = api.event.byId.useQuery({ id: eventId });
+    const { data: club } = api.club.byId.useQuery({ id: event ? event.clubId : "" });
 
-  return (
-    <>
-      <Head>
-        <title>{event.name} - Jupiter</title>
-        <meta name="description">{event.name} - Jupiter</meta>
-      </Head>
-      <main className="w-full md:pl-72">
-        <EventHeader />
-        <div className="mb-5 flex flex-col space-y-6 px-7">
-          <div className="relative h-full w-full rounded-xl bg-slate-50 p-10 shadow-lg">
-            <Image src={club.image} width={100} height={100} alt="club image" />
-            <hr className="my-3 mt-5 h-[1px] border-0 bg-black" />
-            <div className="mb-3 flex w-full justify-between">
-              <div>
-                <a
-                  className="text-4xl font-semibold"
-                  href={`/directory/${club.id}`}
-                >
-                  {club.name}
-                </a>
-                <p className="text-2xl font-semibold">{event.name}</p>
-              </div>
-              <p className="text-2xl font-semibold">
-                {new Date(event.startTime).toString().substring(0, 24)}
-              </p>
-            </div>
-            <p>{event.description}</p>
-            <div className="float-right mt-24">
-              <button className="mr-3 rounded-lg border-[1px] border-black bg-white px-3 py-1">
-                Contact Us
-              </button>
-              <button className="rounded-lg border-[1px] border-black bg-white px-3 py-1">
-                Register
-              </button>
-            </div>
-          </div>
+    if (!event) {
+        return <div>
+            Event Not Found.
         </div>
-      </main>
-    </>
-  );
+    } else if (!club) {
+        return <div>
+            Club Does Not Exist.
+        </div>
+    }
+
+    return (<>
+        <Head>
+            <title>{event.name} - Jupiter</title>
+            <meta name="description">{event.name} - Jupiter</meta>
+        </Head>
+        <main className="w-full md:pl-72">
+            <EventHeader />
+            <div className="mb-5 flex flex-col space-y-6 px-7">
+                <div className="w-full h-full rounded-xl bg-slate-50 shadow-lg p-10 relative">
+                    <Image
+                        src={club.image}
+                        width={100}
+                        height={100}
+                        alt="club image"
+                    />
+                    <hr className="border-0 bg-black h-[1px] my-3 mt-5" />
+                    <div className="w-full flex justify-between mb-3">
+                        <div>
+                            <a className="font-semibold text-4xl" href={`/directory/${club.id}`}>{club.name}</a>
+                            <p className="font-semibold text-2xl">{event.name}</p>
+                        </div>
+                        <p className="font-semibold text-2xl">{new Date(event.startTime).toString().substring(0, 24)}</p>
+                    </div>
+                    <p>
+                        {event.description}
+                    </p>
+                    <div className="float-right mt-24">
+                        <button className="py-1 px-3 mr-3 rounded-lg border-black border-[1px] bg-white">
+                            Contact Us
+                        </button>
+                        <button className="py-1 px-3 rounded-lg border-black border-[1px] bg-white">
+                            Register
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </>);
 };
 export default EventPage;
 
-import {
-  type InferGetServerSidePropsType,
-  type GetServerSideProps,
-} from 'next';
+import { api } from "@src/utils/api";
+import { db } from "@src/server/db";
+import { appRouter } from "@src/server/api/root";
+import { createInnerTRPCContext } from "@src/server/api/trpc";
+import { createServerSideHelpers } from "@trpc/react-query/server";
+import { type GetServerSidePropsContext, type GetStaticPaths, type InferGetStaticPropsType } from "next";
 
-import { db } from '@src/server/db';
-import { eq } from 'drizzle-orm';
-import { club, events } from '@src/server/db/schema';
-import { type SelectClub, type SelectEvent } from '@src/server/db/models';
+export const getStaticPaths: GetStaticPaths = async () => {
+    const events = await db.query.events.findMany();
 
-export const getServerSideProps: GetServerSideProps<{
-  club?: SelectClub;
-  event: SelectEvent;
-}> = async ({ params }) => {
-  const matchedEvent = await db.query.events.findFirst({
-    where: eq(events.id, params?.eventId as string),
-  });
+    if (!events) {
+        return {
+            paths: [],
+            fallback: false,
+        }
+    }
 
-  if (!matchedEvent) return { notFound: true };
+    const paths = events.map(event => ({
+        params: {
+            eventId: event.id,
+        }
+    }));
 
-  const matchedClub = await db.query.club.findFirst({
-    where: eq(club.id, matchedEvent.clubId),
-  });
+    return {
+        paths,
+        fallback: false
+    };
+}
 
-  if (!matchedClub) return { props: { club: undefined, event: matchedEvent } };
+export const getStaticProps = (
+    ctx: GetServerSidePropsContext<{eventId: string}>
+) => {
+    const helper = createServerSideHelpers({
+        router: appRouter,
+        ctx: createInnerTRPCContext({ session: null }),
+    });
 
-  return {
-    props: {
-      club: matchedClub,
-      event: matchedEvent,
-    },
-  };
-};
+    const eventId = ctx.params?.eventId;
+    if (typeof eventId !== 'string') {
+        return {
+            notFound: true,
+        }
+    }
+
+    // Date object provided, causes error
+    // await helper.event.byId.prefetch({ id: eventId });
+    return {
+        props: {
+            trpcState: helper.dehydrate(),
+            eventId,
+        }
+    }
+}
