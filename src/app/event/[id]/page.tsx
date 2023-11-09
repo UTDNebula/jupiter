0368eaf9589b77b2ee8ce68fbb194cbf87ca4f5c
Ethan Bickel <notethanbickel@gmail.com>
2023-11-09 13:27:43 -0600

    remove Head tag

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -3,7 +3,6 @@ import { db } from '@src/server/db';
 import { events } from '@src/server/db/schema';
 import { eq } from 'drizzle-orm';
 import { type Metadata } from 'next';
-import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -20,53 +19,38 @@ export default async function EventsPage({ params }: Params) {
   const { club, ...event } = res;
 
   return (
-    <>
-      <Head>
-        <title>{event.name} - Jupiter</title>
-        <link
-          rel="canonical"
-          href={'https://jupiter.utdnebula.com/' + event.name}
-          key="canonical"
-        />
-        <meta
-          property="og:url"
-          content={'https://jupiter.utdnebula.com/' + event.name}
-        />
-        <meta name="description" content={`${event.name} - Jupiter`} />
-      </Head>
-      <main className="w-full md:pl-72">
-        <EventHeader />
-        <div className="mb-5 flex flex-col space-y-6 px-7">
-          <div className="relative h-full w-full rounded-xl bg-slate-50 p-10 shadow-lg">
-            <Image src={club.image} width={100} height={100} alt="club image" />
-            <hr className="my-3 mt-5 h-[1px] border-0 bg-black" />
-            <div className="mb-3 flex w-full justify-between">
-              <div>
-                <Link
-                  className="text-4xl font-semibold"
-                  href={`/directory/${club.id}`}
-                >
-                  {club.name}
-                </Link>
-                <p className="text-2xl font-semibold">{event.name}</p>
-              </div>
-              <p className="text-2xl font-semibold">
-                {new Date(event.startTime).toString().substring(0, 24)}
-              </p>
+    <main className="w-full md:pl-72">
+      <EventHeader />
+      <div className="mb-5 flex flex-col space-y-6 px-7">
+        <div className="relative h-full w-full rounded-xl bg-slate-50 p-10 shadow-lg">
+          <Image src={club.image} width={100} height={100} alt="club image" />
+          <hr className="my-3 mt-5 h-[1px] border-0 bg-black" />
+          <div className="mb-3 flex w-full justify-between">
+            <div>
+              <Link
+                className="text-4xl font-semibold"
+                href={`/directory/${club.id}`}
+              >
+                {club.name}
+              </Link>
+              <p className="text-2xl font-semibold">{event.name}</p>
             </div>
-            <p>{event.description}</p>
-            <div className="float-right mt-24">
-              <button className="mr-3 rounded-lg border-[1px] border-black bg-white px-3 py-1">
-                Contact Us
-              </button>
-              <button className="rounded-lg border-[1px] border-black bg-white px-3 py-1">
-                Register
-              </button>
-            </div>
+            <p className="text-2xl font-semibold">
+              {new Date(event.startTime).toString().substring(0, 24)}
+            </p>
+          </div>
+          <p>{event.description}</p>
+          <div className="float-right mt-24">
+            <button className="mr-3 rounded-lg border-[1px] border-black bg-white px-3 py-1">
+              Contact Us
+            </button>
+            <button className="rounded-lg border-[1px] border-black bg-white px-3 py-1">
+              Register
+            </button>
           </div>
         </div>
-      </main>
-    </>
+      </div>
+    </main>
   );
 }
 
