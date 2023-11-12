17feafee4c5d68984002ab1266faa9c382293ff6
MistahSanta <jonathanle1111@gmail.com>
2023-11-12 00:25:38 -0600

    top-half

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -19,13 +19,24 @@ export default async function EventsPage({ params }: Params) {
 
   const { club, ...event } = res;
 
+  const clubTags = ['Software', 'Innovation', 'Other']
+
   return (
     
     <main className="w-full md:pl-72 text-white">
       <EventHeader />
       <div className="mb-5 flex flex-col space-y-6 px-7">
         <div className="relative h-full w-full rounded-xl bg-blue-500 p-10 shadow-lg">
-
+          <div className="flex ">
+            {clubTags.map( (tag) => (
+              <p
+                key={tag}
+                className=" mr-5 pt-8 pb-10 font-semibold "
+              >
+                {tag}
+              </p>
+            ))}
+          </div>
           <h1 className="text-4xl font-semibold">
             {event.name}
           </h1>
