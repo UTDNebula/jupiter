34c12e4ee78d7be83c4ee38cdfb1770fd9a68429
Ethan Bickel <notethanbickel@gmail.com>
2023-11-15 12:30:25 -0600

    Update provider buttons

--- src/components/ProviderButtons.tsx
+++ src/components/ProviderButtons.tsx
@@ -6,15 +6,16 @@ import AuthIcons from '@src/icons/AuthIcons';
 type Provider = ClientSafeProvider;
 
 const ProviderButton: FC<{ provider: Provider }> = ({ provider }) => (
-  <div>
-    <button
-      onClick={() => {
-        void signIn(provider.id);
-      }}
-    >
-      {AuthIcons[provider.id]}
-    </button>
-  </div>
+  <button
+    type="button"
+    onClick={() => {
+      void signIn(provider.id);
+    }}
+    className="box-content flex flex-row items-center space-x-4 rounded-3xl bg-white py-2.5 pl-5 pr-6 shadow-sm shadow-slate-700"
+  >
+    <div>{AuthIcons[provider.id]}</div>
+    <h2 className="text-xs font-extrabold text-red-500">{provider.name}</h2>
+  </button>
 );
 
 export default ProviderButton;
