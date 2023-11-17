63f59575858af99d2f3d2e469339627761b5516b
Ethan Bickel <notethanbickel@gmail.com>
2023-11-16 21:19:48 -0600

    trying my best to simplify the css

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -39,11 +39,12 @@ const Carousel = () => {
     <div className="mx-auto w-full">
       <div className="relative aspect-video w-full overflow-hidden rounded-lg">
         {clubs.map((club, key) => (
-          <div
+          <Link
             className={`absolute inset-0 z-0 h-full w-full transition-opacity ${
               key === slide ? 'block' : 'hidden'
             }`}
             key={key}
+            href={`/directory/${club.id}`}
           >
             <div className="relative h-full w-full">
               <Image
@@ -54,11 +55,8 @@ const Carousel = () => {
                 className="h-full w-full rounded-lg object-cover"
                 priority
               />
-              <div className="absolute left-0 top-0 h-full w-full">
-                <Link
-                  className="absolute left-0 top-0 z-20 h-full w-full"
-                  href={`/directory/${club.id}`}
-                >
+              <div className="absolute inset-0 h-full w-full">
+                <div className="absolute inset-0 z-10 h-full w-full">
                   <div className="relative flex h-full w-full flex-col justify-between p-2 md:p-[3.75rem]">
                     <div className="mt-auto flex flex-row justify-center">
                       <h1 className="text-2xl font-bold text-white">
@@ -69,11 +67,11 @@ const Carousel = () => {
                       </div>
                     </div>
                   </div>
-                </Link>
-                <div className="absolute bottom-0 left-0 z-10 h-2/5 w-full bg-gradient-to-t from-gray-500 to-transparent"></div>
+                </div>
+                <div className="absolute bottom-0 left-0 h-2/5 w-full bg-gradient-to-t from-gray-500 to-transparent"></div>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
         <div className="pointer-events-none absolute inset-0 flex h-full w-full flex-col justify-between p-2 md:p-[3.75rem]">
           <div className="pointer-events-auto w-fit rounded-[1.25rem] bg-black bg-opacity-50 px-5 py-2.5 text-center text-xs font-extrabold text-white md:text-base">
