b4d5846afa73d50c95631b81689dd61f2d6a0b9d
MistahSanta <jonathanle1111@gmail.com>
2023-11-17 18:47:09 -0600

    eventPage

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -28,53 +28,7 @@ export default async function EventsPage({ params }: Params) {
 
 
   return (
-    
-    <main className="w-full md:pl-72 text-white">
-  //   <div className="relative">
-  //   <div className="h-full w-full">
-  //     <Image
-  //       src={club.image}
-  //       alt="Picture of the club"
-  //       width={400}
-  //       height={150}
-  //       className="rounded-lg object-cover"
-  //       priority
-  //     />
-  //   </div>
-  //   <div className="absolute left-0 top-0 h-full w-full">
-  //     <div className="flex h-full w-full flex-row   p-8">
-  //       <div className="flex h-full flex-col">
-  //         <div className="flex flex-row">
-  //           {/* {['Software', 'Innovation', 'Other'].map((tag) => (
-  //             <p
-  //               key={tag}
-  //               className="m-2 rounded-full bg-black bg-opacity-50 px-4 py-2 font-semibold text-slate-100"
-  //             >
-  //               {tag}
-  //             </p>
-  //           ))} */}
-  //         </div>
-  //         <h1 className="mt-auto w-fit rounded-full bg-black bg-opacity-50 p-2 text-center text-4xl font-bold text-slate-100">
-  //           {club.name}
-  //         </h1>
-  //       </div>
-  //       <div className="ml-auto flex h-min flex-row items-center gap-x-12 self-center">
-  //         <button className="rounded-full bg-blue-primary px-8 py-4 text-xs font-extrabold text-white transition-colors hover:bg-blue-700">
-  //           Join
-  //         </button>
-  //         <button
-  //           className="rounded-full bg-blue-primary p-2.5 transition-colors hover:bg-blue-700"
-  //           type="button"
-  //         >
-  //           <div className={'h-8 w-8'}>
-              
-  //           </div>
-  //         </button>
-         
-  //       </div>
-  //     </div>
-  //   </div>
-  // </div>
+  
     <main className="w-full md:pl-72">
       <EventHeader />
 
