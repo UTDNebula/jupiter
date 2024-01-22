00b02776ab52713ed5190442efbcb7836c516f9c
Ethan Bickel <notethanbickel@gmail.com>
2024-01-22 15:57:58 -0600

    change get officers to be public

--- src/server/api/routers/club.ts
+++ src/server/api/routers/club.ts
@@ -220,7 +220,7 @@ export const clubRouter = createTRPCRouter({
       );
       return clubId;
     }),
-  getOfficers: protectedProcedure
+  getOfficers: publicProcedure
     .input(byIdSchema)
     .query(async ({ input, ctx }) => {
       const officers = await ctx.db.query.userMetadataToClubs.findMany({
