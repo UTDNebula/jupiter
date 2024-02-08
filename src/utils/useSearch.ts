ab6c89e3960e17e987d9cf2f5c8c33caa0fd4c8c
Ethan Bickel <notethanbickel@gmail.com>
2024-02-08 17:58:57 -0600

    fix typing for searchParams encoder

--- src/utils/useSearch.ts
+++ src/utils/useSearch.ts
@@ -3,13 +3,15 @@ import { useRouter } from 'next/navigation';
 import { useEffect, useReducer } from 'react';
 import { type z } from 'zod';
 
-function EncodeParams<
+export function EncodeParams<
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  T extends Record<string, string | Array<any> | Record<string, any>>,
+  T extends Record<string, any>,
 >(obj: T): string {
   return Object.entries(obj)
     .map((value) =>
-      typeof value[1] === 'string'
+      typeof value[1] === 'string' ||
+      typeof value[1] === 'number' ||
+      typeof value[1] === 'boolean'
         ? `${value[0]}=${encodeURIComponent(value[1])}`
         : `${value[0]}=${encodeURIComponent(JSON.stringify(value[1]))}`,
     )
