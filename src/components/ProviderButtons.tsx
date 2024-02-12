2b7347b0d14733523508e37c0335c09726050417
Ethan Bickel <notethanbickel@gmail.com>
2024-02-12 17:13:33 -0600

    make auth text bigger

--- src/components/ProviderButtons.tsx
+++ src/components/ProviderButtons.tsx
@@ -19,7 +19,9 @@ const ProviderButton: FC<{ provider: Provider }> = ({ provider }) => (
     className="box-content flex flex-row items-center space-x-4 rounded-3xl bg-white py-2.5 pl-5 pr-6 shadow-sm shadow-slate-700"
   >
     <div>{AuthIcons[provider.id]}</div>
-    <h2 className={`text-xs font-extrabold ${colors[provider.name]}`}>
+    <h2
+      className={`text-base font-extrabold md:text-xs ${colors[provider.name]}`}
+    >
       {provider.name}
     </h2>
   </button>
