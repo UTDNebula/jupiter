397e2cdb6c93019d9492f65f58a37e12808207c6
Ruben Olano <rubendavid2711@gmail.com>
2024-02-16 17:35:13 -0600

    Remove class that messes up last card in events page in list view

--- src/app/events/eventView.tsx
+++ src/app/events/eventView.tsx
@@ -14,13 +14,13 @@ const EventView = ({ events, params }: Props) => {
 
   return (
     <div className="w-full px-6">
-      <div className="flex flex-col pt-4 sm:flex-row sm:pb-12 md:pr-7.5">
+      <div className="md:pr-7.5 flex flex-col pt-4 sm:flex-row sm:pb-12">
         <h1 className="text-left text-2xl font-bold text-[#4D5E80]">Events</h1>
         <div className="relative z-0 hidden flex-row gap-x-16 md:ml-auto md:flex">
           <Link
             className={`z-20 flex flex-row items-center gap-x-4 ${
               view === 'list'
-                ? ' -mr-7.5 rounded-full bg-white px-7.5 py-2.5'
+                ? ' -mr-7.5 px-7.5 rounded-full bg-white py-2.5'
                 : ''
             }`}
             href={{
@@ -34,12 +34,12 @@ const EventView = ({ events, params }: Props) => {
             <div className="h-7.5 w-7.5">
               <ListIcon />
             </div>
-            <p className="text-sm font-bold text-blue-primary">List view</p>
+            <p className="text-blue-primary text-sm font-bold">List view</p>
           </Link>
           <Link
             className={`z-20 flex flex-row items-center gap-x-4 ${
               view == 'grid'
-                ? '-ml-7.5 rounded-full bg-white px-7.5 py-2.5'
+                ? '-ml-7.5 px-7.5 rounded-full bg-white py-2.5'
                 : 'mr-7.5'
             }`}
             href={{
@@ -53,18 +53,18 @@ const EventView = ({ events, params }: Props) => {
             <div className="h-7.5 w-7.5">
               <GridIcon />
             </div>
-            <p className="text-sm font-bold text-blue-primary">Grid view</p>
+            <p className="text-blue-primary text-sm font-bold">Grid view</p>
           </Link>
         </div>
       </div>
-      <div className="container flex w-full flex-col overflow-x-clip sm:flex-row sm:space-x-7.5">
+      <div className="sm:space-x-7.5 container flex w-full flex-col overflow-x-clip sm:flex-row">
         <EventSidebar />
         <div
           data-view={view}
           className={
             view === 'list'
-              ? 'group flex flex-col items-center space-y-7.5 pt-10 sm:justify-start'
-              : 'group flex flex-wrap justify-center gap-x-10 gap-y-7.5 pt-10 sm:justify-start'
+              ? 'space-y-7.5 group flex flex-col pt-10 sm:justify-start'
+              : 'gap-y-7.5 group flex flex-wrap justify-center gap-x-10 pt-10 sm:justify-start'
           }
         >
           {events.map((event) => {
