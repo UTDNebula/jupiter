340adaca1fc5663361c726dbe1554ef5b71f680b
Jonathan <jonathanle1111@gmail.com>
2024-02-23 01:31:58 -0600

    added better ui for feedback page

--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -4,6 +4,7 @@ import Header from '@src/components/BaseHeader';
 import { type Metadata } from 'next';
 import Image from 'next/image';
 import jupiterPic from "assets/media/icons/Jupiter.png"
+import nebulaPic from "public/android-chrome-192x192.png"
 
 export const metadata: Metadata = {
   title: 'Feedback - Jupiter',
@@ -19,12 +20,12 @@ export const metadata: Metadata = {
 
 const Feedback = () => {
   
-  function handleSubmit( e : React.FormEvent<HTMLFormElement>) {
-    e.preventDefault() 
-    console.log("Form submitted")
-    // Add a POST request to send the feedback to the server -- Add later 
+  // function handleSubmit( e : React.FormEvent<HTMLFormElement>) {
+  //   e.preventDefault() 
+  //   console.log("Form submitted")
+  //   // Add a POST request to send the feedback to the server -- Add later 
 
-  }
+  // }
 
 
   return (
@@ -41,35 +42,42 @@ const Feedback = () => {
           />
         </section>
 
-        <section className='bg-white m-auto justift-center items-center text-center p-6 shadow-lg rounded-lg'>
-          <form >
-            <h1 className=" text-4xl font-bold " >Feedback</h1> 
+        <section className='bg-white m-auto justift-center items-center text-center py-6 px-10 shadow-lg rounded-lg'>
+          <form className='relative  text-slate-700'>
+            <div className="w-14 h-14 absolute -top-3 left-0 ">
+              <Image 
+                src={nebulaPic}
+                alt="Nebula"
+                fill 
+              />
+            </div>
+            <h1 className=" text-4xl font-bold pb-2" >Feedback</h1> 
             
-            <h3 className="text-md border-t-2 py-4">
+            <h3 className="text-md border-t-2 border-black py-4 text-slate-500">
               On a scale of 1-5, how would you rate your experience with Jupiter?
             </h3>
-            <input type="number" min="1" max="5" step="1" className=" shadow-xl border-gray-500 border-2 rounded-md" />
+            <textarea className="w-4/5 h-15 p-1 text-left  shadow-xl border-gray-500 border-2 rounded-md"></textarea>
             {/* Add a rating system here */}
 
             <br></br>
-
-            <h3 className="text-md border-t-2 py- my-4">
+            
+            <h3 className="text-md border-t-2 border-slate-200 pt-2 my-4 text-slate-500">
               What do you like about Jupiter?
             </h3>
-            <input type="text w-full " className=" shadow-xl border-gray-500 border-2 rounded-md"/>
+            <textarea className=" overflow-auto w-4/5 h-15 p-1 text-left  shadow-xl border-gray-500 border-2 rounded-md"></textarea>
             <br></br>
 
-            <h3 className="text-md border-t-2 py-4 my-4 w-full">
+            <h3 className="text-md border-t-2 border-slate-200 pt-2 my-4 w-full text-slate-500">
               What do you dislike about Jupiter?
             </h3>
   
-            <input type="text w-full" className=" shadow-xl border-gray-500 border-2 rounded-md"/>
+            <textarea className="w-4/5 h-15 p-1 text-left  shadow-xl border-gray-500 border-2 rounded-md"></textarea>
   
-            <h3 className="text-md border-t-2 py-4 my-4 ">
+            <h3 className="text-md border-t-2 border-slate-200 text-slate-500 pt-2 my-4 ">
               What features would you like to see in Jupiter?
             </h3>
             
-            <input type='text w-full' className=" shadow-xl border-gray-500 border-2 rounded-md"></input>
+            <textarea className="w-4/5 h-15 p-1 text-left  shadow-xl border-gray-500 border-2 rounded-md"></textarea>
             <br></br> 
             <button type="submit" className="mx-auto my-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
               Submit
