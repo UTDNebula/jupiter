d92234bec4347e675552ce1287f8c7959f51b563
Ethan Bickel <notethanbickel@gmail.com>
2024-03-08 19:54:36 -0600

    add seperate Contact button option for emails

--- src/components/ContactButtons.tsx
+++ src/components/ContactButtons.tsx
@@ -2,6 +2,19 @@ import type { SelectContact as Contacts } from '@src/server/db/models';
 import { logo } from './ContactIcons';
 import Link from 'next/link';
 
+const EmailButton = ({ item }: { item: Contacts }) => {
+  return (
+    <button
+      key={item.url}
+      className="group relative h-min self-center rounded-full bg-slate-100 p-2.5 transition-colors hover:bg-blue-700"
+    >
+      <Link href={`mailto:${item.url}`}>
+        <div className="relative h-8 w-8">{logo[item.platform]}</div>
+      </Link>
+    </button>
+  );
+};
+
 type contentButtonProps = {
   contacts: Array<Contacts>;
 };
@@ -9,14 +22,20 @@ const ContactButtons = ({ contacts }: contentButtonProps) => {
   return (
     <div className="flex flex-row content-center gap-x-4">
       {contacts.map((item) => (
-        <button
-          key={item.url}
-          className="group relative h-min self-center rounded-full bg-slate-100 p-2.5 transition-colors hover:bg-blue-700"
-        >
-          <Link href={item.url} target="_blank">
-            <div className="relative h-8 w-8">{logo[item.platform]}</div>
-          </Link>
-        </button>
+        <>
+          {item.platform === 'email' ? (
+            <EmailButton item={item} />
+          ) : (
+            <button
+              key={item.url}
+              className="group relative h-min self-center rounded-full bg-slate-100 p-2.5 transition-colors hover:bg-blue-700"
+            >
+              <Link href={item.url} target="_blank">
+                <div className="relative h-8 w-8">{logo[item.platform]}</div>
+              </Link>
+            </button>
+          )}
+        </>
       ))}
     </div>
   );
