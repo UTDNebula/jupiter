b2136e982f8c3a99d3195e7e6ea9d654a81a71ae
Ethan Bickel <notethanbickel@gmail.com>
2024-03-22 19:51:32 -0500

    remove list and grid toggle

--- src/app/events/eventView.tsx
+++ src/app/events/eventView.tsx
@@ -1,59 +1,24 @@
 'use client';
 import EventSidebar from '@src/components/events/EventSidebar';
-import { GridIcon, ListIcon } from '@src/icons/Icons';
-import { eventParamsSchema } from '@src/utils/eventFilter';
-import useSearch from '@src/utils/useSearch';
 import { type ReactNode } from 'react';
 type Props = {
   children: ReactNode;
 };
 
 const EventView = ({ children }: Props) => {
-  const [{ view }, setParams] = useSearch(eventParamsSchema);
-
   return (
     <div className="w-full px-6">
       <div className="flex flex-row pb-12 pr-7.5 pt-4">
         <h1 className="text-2xl font-bold text-[#4D5E80]">Events</h1>
         <div className="relative z-0 ml-auto flex flex-row gap-x-16">
-          <button
-            className={`z-20 flex flex-row items-center gap-x-4 ${
-              view === 'list'
-                ? ' -mr-7.5 rounded-full bg-white px-7.5 py-2.5'
-                : ''
-            }`}
-            type="button"
-            onClick={() => setParams({ view: 'list' })}
-          >
-            <div className="h-7.5 w-7.5">
-              <ListIcon />
-            </div>
-            <p className="text-sm font-bold text-blue-primary">List view</p>
-          </button>
-          <button
-            className={`z-20 flex flex-row items-center gap-x-4 ${
-              view === 'grid'
-                ? '-ml-7.5 rounded-full bg-white px-7.5 py-2.5'
-                : 'mr-7.5'
-            }`}
-            onClick={() => setParams({ view: 'grid' })}
-          >
-            <div className="h-7.5 w-7.5">
-              <GridIcon />
-            </div>
-            <p className="text-sm font-bold text-blue-primary">Grid view</p>
-          </button>
+          date picker here
         </div>
       </div>
       <div className="container flex w-full flex-row space-x-7.5">
         <EventSidebar />
         <div
-          data-view={view}
-          className={
-            view === 'list'
-              ? 'group flex w-full flex-col space-y-7.5 pt-10'
-              : 'group flex flex-wrap gap-x-10 gap-y-7.5 pt-10'
-          }
+          data-view={'list'}
+          className={'group flex w-full flex-col space-y-7.5 pt-10'}
         >
           {children}
         </div>
