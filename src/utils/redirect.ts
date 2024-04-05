d3cf48bb58006516ee2586a955fcce88319b7a98
Ethan Bickel <notethanbickel@gmail.com>
2024-04-05 17:19:57 -0500

    fix: auth redirects not working right on prod

--- src/utils/redirect.ts
+++ src/utils/redirect.ts
@@ -1,7 +1,8 @@
-import { env } from '@src/env.mjs';
-
 export function signInRoute(route: string) {
-  return `/auth?callbackUrl=${encodeURIComponent(
-    `${env.NEXTAUTH_URL}/${route}`,
-  )}`;
+  return `/auth?callbackUrl=${encodeURIComponent(`${getBaseUrl()}/${route}`)}`;
+}
+export function getBaseUrl() {
+  if (typeof window !== 'undefined') return '';
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  return `http://localhost:${process.env.PORT ?? 3000}`;
 }
