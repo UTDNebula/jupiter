2f48274a9ebb806a1b7f6e8dc9e22d343dd3aa7a
Ethan Bickel <notethanbickel@gmail.com>
2024-04-18 15:32:54 -0500

    update trpc to v11 and react-query to v5

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -2,9 +2,12 @@ import { headers } from 'next/headers';
 
 import { createCaller } from '@src/server/api/root';
 import { createTRPCContext } from '@src/server/api/trpc';
+import { cache } from 'react';
 
-const heads = new Headers(headers());
-heads.set('x-trpc-source', 'rsc');
-const createContext = await createTRPCContext({ headers: heads });
+const createContext = cache(() => {
+  const heads = new Headers(headers());
+  heads.set('x-trpc-source', 'rsc');
+  return createTRPCContext({ headers: heads });
+});
 
 export const api = createCaller(createContext);
