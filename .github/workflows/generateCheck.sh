8f3764e5ff144c389079ce2aad2c1c0e54cac7f6
Ethan Bickel <notethanbickel@gmail.com>
2024-04-24 17:01:31 -0500

    fix permissions for script
    actually fix

--- .github/workflows/generateCheck.sh
+++ .github/workflows/generateCheck.sh
@@ -2,7 +2,7 @@ if [ $status == 0]; then
 	echo "## Formatting Check passed 🥳" >>$GITHUB_STEP_SUMMARY
 	echo "All files are formatted correctly" >>$GITHUB_STEP_SUMMARY
 else
-	echo "## Formatting Check Failed" >>$GITHUB_STEP_SUMMARY
+	echo "## Formatting Check Failed 😅" >>$GITHUB_STEP_SUMMARY
 	echo "Please run prettier using \`npx prettier . --write\` in order to format your code" >>$GITHUB_STEP_SUMMARY
 	echo "### Files with bad formatting:" >>$GITHUB_STEP_SUMMARY
 	for file in $files; do
