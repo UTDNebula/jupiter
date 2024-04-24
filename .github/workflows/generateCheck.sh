7f6c7f02236e5a2e327d99526574916a3558d77e
Ethan Bickel <notethanbickel@gmail.com>
2024-04-24 18:21:01 -0500

    make check fail

--- .github/workflows/generateCheck.sh
+++ .github/workflows/generateCheck.sh
@@ -1,12 +1,14 @@
 if [ $status == 0 ]; then
 	echo "## Formatting Check passed 🥳" >>$GITHUB_STEP_SUMMARY
 	echo "All files are formatted correctly" >>$GITHUB_STEP_SUMMARY
+	exit 0
 else
 	echo "## Formatting Check Failed 😅" >>$GITHUB_STEP_SUMMARY
 	echo "Please run prettier using \`npx prettier . --write\` in order to format your code" >>$GITHUB_STEP_SUMMARY
 	echo "### Files with bad formatting:" >>$GITHUB_STEP_SUMMARY
 	for file in $files; do
 		echo "- $file" >>$GITHUB_STEP_SUMMARY
-		echo "::error file=$file::File Not Formatted correctly"
+		echo "::error file=$file::$file Not Formatted correctly"
 	done
+	exit 1
 fi
