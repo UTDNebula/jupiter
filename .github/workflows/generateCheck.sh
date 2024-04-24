042aad95a498b594860c908c4a3a1d8bf56b7b72
Ethan Bickel <notethanbickel@gmail.com>
2024-04-24 17:59:26 -0500

    fix: error out on failed formatting before error message

--- .github/workflows/generateCheck.sh
+++ .github/workflows/generateCheck.sh
@@ -1,6 +1,7 @@
 if [ $status == 0 ]; then
 	echo "## Formatting Check passed 🥳" >>$GITHUB_STEP_SUMMARY
 	echo "All files are formatted correctly" >>$GITHUB_STEP_SUMMARY
+	exit 0
 else
 	echo "## Formatting Check Failed 😅" >>$GITHUB_STEP_SUMMARY
 	echo "Please run prettier using \`npx prettier . --write\` in order to format your code" >>$GITHUB_STEP_SUMMARY
@@ -8,4 +9,5 @@ else
 	for file in $files; do
 		echo "- $file" >>$GITHUB_STEP_SUMMARY
 	done
+	exit 1
 fi
