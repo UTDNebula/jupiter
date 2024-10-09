abb83ecd99c1780f8d758997b9d1990d4387899f
Ethan Bickel <notethanbickel@gmail.com>
2024-10-09 10:51:14 -0500

    attempt 3 at fixing bad output

--- .github/workflows/generateCheck.sh
+++ .github/workflows/generateCheck.sh
@@ -1,4 +1,4 @@
-if [ $files == '' ]; then
+if [ -z "${files}" ]; then
   echo "## Formatting Check passed 🥳" >>$GITHUB_STEP_SUMMARY
   echo "All files are formatted correctly" >>$GITHUB_STEP_SUMMARY
   exit 0
