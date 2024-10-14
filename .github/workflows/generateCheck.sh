628f95cb702831447d5f28b481f9c55e924929dc
Ethan Bickel <notethanbickel@gmail.com>
2024-10-14 09:23:20 -0500

    bad variable

--- .github/workflows/generateCheck.sh
+++ .github/workflows/generateCheck.sh
@@ -1,6 +1,6 @@
 files=$(npx prettier . -l)
 status=$?
-if [ status == 0 ]; then
+if [ $status == 0 ]; then
   echo "## Formatting Check passed 🥳" >>$GITHUB_STEP_SUMMARY
   echo "All files are formatted correctly" >>$GITHUB_STEP_SUMMARY
   exit 0
