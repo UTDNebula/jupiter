59459795b208f3a510b0d91bb2124b59e7a08624
Ethan Bickel <notethanbickel@gmail.com>
2024-10-22 12:50:18 -0500

    make hamburger white

--- src/components/landing/LandingPage.tsx
+++ src/components/landing/LandingPage.tsx
@@ -22,7 +22,7 @@ const LandingPage = () => {
         className="absolute inset-0 z-10 w-full"
       >
         <div className="z-20 flex h-20 w-full flex-row items-center">
-          <Sidebar />
+          <Sidebar hamburger="white" />
           <div>
             <Image
               src="/nebula-logo.png"
@@ -30,12 +30,16 @@ const LandingPage = () => {
               width={60}
               height={60}
               className="mr-1.5"
+              priority
             />
           </div>
         </div>
       </div>
-      <div className="pointer-events-none absolute inset-0 z-20 flex h-full w-full flex-col items-center justify-center gap-y-5">
-        <h1 className="pointer-events-auto max-w-3xl text-center font-sans text-6xl font-semibold text-white">
+      <div className="pointer-events-none absolute inset-0 z-20 flex h-full w-full flex-col items-center justify-center">
+        <h2 className="pointer-events-auto mb-2 max-w-2xl text-xl text-white md:text-3xl">
+          Find your Community
+        </h2>
+        <h1 className="pointer-events-auto mb-4 max-w-3xl px-5 text-center font-sans text-2xl font-semibold text-white md:px-0 md:text-6xl">
           Discover the Best Clubs and Organizations at UTD
         </h1>
         <ClubSearchBar />
