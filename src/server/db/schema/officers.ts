c4a9da0fab22bc4e757d53d3321a1c29756226e5
BK2004 <thunder10.bk@gmail.com>
2024-10-23 16:47:21 -0500

    Removed title from users -> club relation; Added officers table for listed club officers -- is_president flag for officers that can't be removed

---
+++ src/server/db/schema/officers.ts
@@ -0,0 +1,24 @@
+import { relations } from 'drizzle-orm';
+import { pgTable, text, boolean, primaryKey } from 'drizzle-orm/pg-core';
+import { club } from './club';
+
+export const officers = pgTable(
+	'officers',
+	{
+		id: text('id').notNull(),
+		clubId: text('club_id')
+			.notNull()
+			.references(() => club.id),
+		name: text('name').notNull(),
+		position: text('position').notNull(),
+		image: text('image').default('/nebula-logo.png').notNull(),
+		isPresident: boolean('is_president').default(false).notNull(),
+	}, 
+	(table) => ({
+		pk: primaryKey(table.clubId, table.id),
+	})
+);
+
+export const officersToClubs = relations(officers, ({ one }) => ({
+	club: one(club, { fields: [officers.clubId], references: [club.id] }),
+}));
