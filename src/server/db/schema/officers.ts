24903aa6c908f3fa8a514fafbe585dbd4546338c
BK2004 <thunder10.bk@gmail.com>
2024-10-29 19:01:42 -0500

    Removed composite key for officers id and clubId; gave officer id a default as uuid

--- src/server/db/schema/officers.ts
+++ src/server/db/schema/officers.ts
@@ -1,23 +1,17 @@
 import { relations } from 'drizzle-orm';
-import { pgTable, text, boolean, primaryKey } from 'drizzle-orm/pg-core';
+import { pgTable, text, boolean, uuid } from 'drizzle-orm/pg-core';
 import { club } from './club';
 
-export const officers = pgTable(
-  'officers',
-  {
-    id: text('id').notNull(),
-    clubId: text('club_id')
-      .notNull()
-      .references(() => club.id),
-    name: text('name').notNull(),
-    position: text('position').notNull(),
-    image: text('image').default('/nebula-logo.png').notNull(),
-    isPresident: boolean('is_president').default(false).notNull(),
-  },
-  (table) => ({
-    pk: primaryKey(table.clubId, table.id),
-  }),
-);
+export const officers = pgTable('officers', {
+  id: uuid('id').defaultRandom().notNull().primaryKey(),
+  clubId: text('club_id')
+    .notNull()
+    .references(() => club.id),
+  name: text('name').notNull(),
+  position: text('position').notNull(),
+  image: text('image').default('/nebula-logo.png').notNull(),
+  isPresident: boolean('is_president').default(false).notNull(),
+});
 
 export const officersToClubs = relations(officers, ({ one }) => ({
   club: one(club, { fields: [officers.clubId], references: [club.id] }),
